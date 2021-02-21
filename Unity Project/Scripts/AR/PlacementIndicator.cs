9fadf39be6f0efaf444387dff07b34bb6132220c
Mohammed Mehboob <mm2260@g.rit.edu>
2021-02-21 04:00:03 -0500

    Added unity project: - Implemented basic AR setup; TODO: Integrate RESTful API, Visual Markers.

---
+++ Unity Project/Scripts/AR/PlacementIndicator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class PlacementIndicator : MonoBehaviour
+{
+
+    private ARRaycastManager _raycastManager;
+    private GameObject _indicatorGameObject;
+
+    [SerializeField] private float movementVelocity = 2f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _raycastManager = FindObjectOfType<ARRaycastManager>();
+        _indicatorGameObject = transform.GetChild(0).gameObject;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        var raycastHits = new List<ARRaycastHit>();
+        _raycastManager.Raycast(
+            new Vector2(Screen.width / 2f,
+                                  Screen.height / 2f),
+            raycastHits,
+            TrackableType.Planes );
+
+        if (raycastHits.Count > 0)
+        {
+            var t = transform;
+            t.position = Vector3.Lerp(t.position, raycastHits[0].pose.position, Time.deltaTime*movementVelocity ); ;
+            t.rotation = raycastHits[0].pose.rotation;
+        }
+        
+    }
+}
---
+++ Unity Project/Scripts/Animations/Rotate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed = 12f;
+    private void Update()
+    {
+        transform.Rotate((Vector3.up + Vector3.right) * (float) ( rotationSpeed * Math.Sin(Time.time) * Time.deltaTime ) );
+    }
+}
